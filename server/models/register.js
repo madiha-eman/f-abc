5c8d27e45d14b13055964914d5e40c021fab3d15
madiha-eman <madihaema1@gmail.com>
2021-03-16 10:02:54 +0500

    'schema'

--- server/models/register.js
+++ server/models/register.js
@@ -1,74 +1,108 @@
 const mongoose = require('mongoose');
 
-const patRegis = mongoose.Schema({
-    mrNO: {
-        type: String,
+const RegisSchema = mongoose.Schema({
+    MRNO: {
+        type: Number,
         required: [true, ""]
     },
-    tokenNo: {
-        type: String,
+    TokenNo: {
+        type: Number,
         required: [true, "Token is required"]
+    },
+    Name: {
+        type: String,
+        required: [true, "Name is required"]
+    },
+    FatherName: {
+        type: String,
+        required: [true, "fatherName is required"]
+    },
+    DOB: {
+        type: new Date(),
+        required: [true, "D.O.B is required"]
+    },
+    Gender: {
+        type: String,
+        required: [true, "gender is required"]
+    },
+    Religion: {
+        type: String,
+        required: [true, "religion is required"]
+    },
+    Help: {
+        type: String,
+        required: [true, "help type is required"]
+    },
+    District: {
+        type: String,
+        required: [true, "CNIC is required"]
+    },
+    CNIC: {
+        type: Number,
+        required: [true, "CNIC is required"]
+    },
+    PhoneNo: {
+        type: Number,
+        required: [true, "CNIC is required"]
+    },
+    Mobile: {
+        type: String,
+        required: [true, "CNIC is required"]
+    },
+    OffPhone: {
+        type: String,
+        required: [true, "CNIC is required"]
+    },
+    RefBy: {
+        type: String,
+        required: [true, "CNIC is required"]
+    },
+    Remarks: {
+        type: String,
+        required: [true, "CNIC is required"]
+    },
+    IsRejected: {
+        required: [false, "CNIC is required"]
+    },
+    IsZakat: {
+        type: String,
+        required: [true, "CNIC is required"]
+    },
+    IsPAFEmp: {
+        required: [false, "CNIC is required"]
+    },
+    RefBy: {
+        type: String,
+        required: [true, "CNIC is required"]
+    },
+    MonthlyConsLimit: {
+        type: Number,
+        required: [true, "CNIC is required"]
+    },
+    NOY: {
+        type: String,
+        required: [true, "CNIC is required"]
+    },
+    EmpId: {
+        type: Number,
+        required: [true, "CNIC is required"]
+    },
+    IsStaff: {
+        type: String,
+        required: [true, "CNIC is required"]
+    },
+    CreateUser: '',
+    ModifyUser:'',
+    ModifyDate: {
+        type: String,
+        required: [true, "CNIC is required"]
     },
     createdAt: {
         type: Date,
         default: new Date()
     },
 
-    name: {
-        type: String,
-        required: [true, "Name is required"]
-    },
-    fatherName: {
-        type: String,
-        required: [true, "fatherName is required"]
-    },
-    dob: {
-        type: String,
-        required: [true, "D.O.B is required"]
-    },
-    gender: {
-        type: String,
-        required: [true, "gender is required"]
-    },
-    religion: {
-        type: String,
-        required: [true, "religion is required"]
-    },
-    help: {
-        type: String,
-        required: [true, "help type is required"]
-    },
-    cnic: {
-        type: Number,
-        required: [true, "CNIC is required"]
-    },
-    cnic: {
-        type: String,
-        required: [true, "CNIC is required"]
-    },
-    cnic: {
-        type: String,
-        required: [true, "CNIC is required"]
-    },
-    cnic: {
-        type: String,
-        required: [true, "CNIC is required"]
-    },
-    cnic: {
-        type: String,
-        required: [true, "CNIC is required"]
-    },
-    cnic: {
-        type: String,
-        required: [true, "CNIC is required"]
-    },
-  
-    // isAdmin:{
-    //     type: Number,
-    //     default: 0
-    // },
-    
 })
 
-const Register = mongoose.model('Register', patRegisSchema)
+const Register = mongoose.model('Register', RegisSchema)
 module.exports = Register
